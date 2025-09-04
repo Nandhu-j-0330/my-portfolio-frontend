7bea9945b41db0b835d7bdd1ee87431a816ef6c1
Nantha kumar J <nan1dhakumar@gmail.com>
2025-09-04 19:55:49 +0530

    fix: removed unused ContactsList import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import ContactsList from "../components/contactsList";
 
 export default function Page() {
   return (
@@ -17,7 +16,6 @@ export default function Page() {
         <Projects />
         <Skills />
         <Contact />
-        {/* <ContactsList /> */}
       </main>
       <Footer />
     </>
